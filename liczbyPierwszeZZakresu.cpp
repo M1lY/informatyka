d152b930d433794089db9979049fd2b209033221
Piotr <pmilewski007@gmail.com>
2018-09-22 00:30:50 +0200

    Liczby pierwsze z zakresu

---
+++ liczbyPierwszeZZakresu.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main(){
+	int n;
+	bool b;
+    cin>>n;
+    for(int i=2; i<=n; i++){
+    	b=1;
+    	for(int j=2; j<=sqrt(i); j++){
+        	if(i%j==0) b=0;
+        }
+        if(b)cout<<i<<" ";
+    }
+    return 0;
+}
