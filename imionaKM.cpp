9030df218cfeec7934ca72ee71845acba37caa6d
Piotr <pmilewski007@gmail.com>
2018-10-03 15:22:03 +0200

    Imie meskie/zenskie

---
+++ imionaKM.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(){
+    string imie;
+    cout<<"Podaj imie: ";
+    cin>>imie;
+    if(imie[imie.length()-1]=='a') cout<<"To imie zenskie";
+    else cout<<"To imie meskie";
+    return 0;
+}
