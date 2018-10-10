10521631f9cccdc1dc1184c0b7906cab2d74895c
Piotr <pmilewski007@gmail.com>
2018-10-10 16:52:05 +0200

    Czy w1 jest anagramem w Char

--- anagramyChar.cpp
+++ anagramyChar.cpp
@@ -6,8 +6,6 @@ bool anagramy(char w[], char w1[]){
 	int d1=strlen(w1);
 	char temp[d1+1];
 	if(d!=d1) return false;
-	//cout<<w<<"*";
-	//cout<<endl<<w1;
 	strcpy(temp,w1);
     bool b;
 	for(int i=0; i<d; i++){
---
+++ anagramyString.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+using namespace std;
+bool anagram(string wyrazA, string wyraz1){
+	int d1=wyraz1.size();
+	int da=wyrazA.size();
+    string temp;
+	if(da!=d1) return false;
+    temp=wyrazA;
+    bool b;
+	for(int i=0; i<d1; i++){
+        b=false;
+		for(int j=0; j<da; j++){
+			if(wyraz1[i]==temp[j]){
+                temp[j]='/0';
+                b=true;
+            }
+		}
+        if(b==false) return false;
+	}
+	return true;
+}
+int main(){
+    string tekst;
+	getline(cin,tekst);
+    tekst+=' ';
+    int d=tekst.size();
+    string wyrazA, wyraz1;
+    int j=0;
+    for(int i=0; i<d; i++){
+        cout<<i;
+        if(tekst[i]!=' '){
+            wyrazA+=tekst[i];
+        }else{
+            if(j==0){
+                wyraz1=wyrazA;
+                cout<<"Anagramy wyrazu: "<<wyrazA<<" to:"<<endl;
+                j++;         
+                wyrazA="";
+                continue;
+            }else if(anagram(wyrazA,wyraz1)){
+                cout<<wyrazA<<endl;
+            }
+            wyrazA="";
+        }
+    }
+	return 0;
+}
