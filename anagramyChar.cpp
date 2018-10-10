cca991fccc3c29a771978b1ee7fc7438064ca2c8
Piotr <pmilewski007@gmail.com>
2018-10-10 16:50:54 +0200

    Czy w1 jest anagramem w Char

--- anagramyChar.cpp
+++ anagramyChar.cpp
@@ -6,6 +6,8 @@ bool anagramy(char w[], char w1[]){
 	int d1=strlen(w1);
 	char temp[d1+1];
 	if(d!=d1) return false;
+	//cout<<w<<"*";
+	//cout<<endl<<w1;
 	strcpy(temp,w1);
     bool b;
 	for(int i=0; i<d; i++){
@@ -14,6 +16,7 @@ bool anagramy(char w[], char w1[]){
 			if(w[i]==temp[j]){
                 temp[j]='/0';
                 b=true;
+				break;
             }
 		}
         if(b==false) return false;
