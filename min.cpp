2461e5a174de157ea8e24e3d877878a27af5ce04
Piotr <pmilewski007@gmail.com>
2018-10-24 20:01:49 +0200

    Minimum z zamianą na binarny i hexadecymalny

--- min.cpp
+++ min.cpp
@@ -21,6 +21,40 @@ int main(){
     for(i=1;i<n;i++){
         if(min>tab[i]) min=tab[i];
     }
-    cout<<endl<<min;
+    cout<<endl<<"minimum: "<<min<<endl;
+    
+    string s;
+    i=0;
+    int temp=min;
+    while(temp>0){
+        s[i]=temp%2;
+        temp/=2;
+        i++;
+    }
+    i--;
+    cout<<"minimum w systemie binarnym: ";
+    while(i>=0){
+        cout<<int(s[i]);
+        i--;
+    }
+
+    s="";
+    i=0;
+    temp=min;
+    while(temp>0){
+        s[i]=temp%16;
+        temp/=16;
+        i++;
+    }
+    i--;
+    cout<<endl<<"minimum w systemie hexadecymalnym: ";
+    while(i>=0){
+        if(int(s[i])<10) cout<<int(s[i]);
+        else{
+            char k=s[i];
+            cout<<char('A'+k-10);
+        }
+        i--;
+    }
     return 0;
 }
