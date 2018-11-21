10f207e947838b5c9193016d1e22c86958f11e62
Piotr <pmilewski007@gmail.com>
2018-11-21 21:53:36 +0100

    Silnia rekurencyjnie

---
+++ silniaRekur.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int silnia(int n){
+    return n==0 ? 1 : n*silnia(n-1);
+}
+int main(){
+    int n;
+    cin>>n;
+    cout<<silnia(n);
+    return 0;
+}
