1d15a75be89e27a7526d0fecd6b00aa33cde1d14
Piotr <pmilewski007@gmail.com>
2018-11-26 21:34:53 +0100

    NWD z dzieleniem rekurencyjnie (euklides)

---
+++ nwdDzielenieRekur.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int nwd(int a, int b){
+    if(b!=0) return nwd(b,a%b);
+    return a;
+}
+int main(){
+    int a,b;
+    cin>>a>>b;
+    cout<<nwd(a,b);
+    return 0;
+}
