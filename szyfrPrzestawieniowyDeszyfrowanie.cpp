5fcda9fe10fd75d211755ec0320d1da792f858df
Piotr <pmilewski007@gmail.com>
2019-01-16 22:59:41 +0100

    Szyfr przestawieniowy

--- szyfrPrzestawieniowyDeszyfrowanie.cpp
+++ szyfrPrzestawieniowyDeszyfrowanie.cpp
@@ -1,14 +1,28 @@
 #include <iostream>
+#include<cmath>
 using namespace std;
 
 string szyfrPrzestawieniowy(string s, int k){
 	string szyfr="";
 	int d=s.size();
-	k%=d;
+	int h=d%k;
+	if(d%k>0) k=ceil((float)d / (float)k);
+	int l=0;
+	h%=d;
 	for(int i=0; i<k; i++){
 		int j=i;
-		for(int j=i; j<d; j+=k){
-			szyfr+=s[j];
+		int g=0;
+		for(int j=i; j<=d; g++){
+			if(l==d) break;
+			if(g<=h){
+				szyfr+=s[j];
+				j+=k;
+			}
+			else{
+				szyfr+=s[j-1];
+				j+=k-1;
+			}
+			l++;
 		}
 	}
 	return szyfr;
