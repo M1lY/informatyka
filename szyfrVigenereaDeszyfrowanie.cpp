e771f7ef68da8c17d0856a7cb120e5999f007ec9
Piotr <pmilewski007@gmail.com>
2019-01-19 12:31:38 +0100

    Syfrowanie Vigenerea - pliki

--- szyfrVigenereaDeszyfrowanie.cpp
+++ szyfrVigenereaDeszyfrowanie.cpp
@@ -2,16 +2,16 @@
 using namespace std;
 
 string szyfrVigener(string s, string k){
-	string szyfr="";
+	string jawny="";
 	int d1=s.size();
 	int d2=k.size();
 	int kod=0;
 	for(int i=0; i<d1; i++){
 		kod=int(s[i])-int(k[i%d2])+65;
-		if(kod>=65) szyfr+=char(kod);
-		else szyfr+=char(kod+26);
+		if(kod>=65) jawny+=char(kod);
+		else jawny+=char(kod+26);
 	}
-	return szyfr;
+	return jawny;
 }
 int main(){
 	string s;
---
+++ szyfrVigenereaPliki/szyfVigenereaPliki.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+ifstream weTekst("dane.txt");
+ifstream weKlucz("klucz.txt");
+ofstream wy("wynik");
+
+string szyfrVigener(string s, string k){
+	string szyfr="";
+	int d1=s.size();
+	int d2=k.size();
+	int kod=0;
+	for(int i=0; i<d1; i++){
+		kod=int(s[i])+int(k[i%d2])-65;
+		if(kod<=90) szyfr+=char(kod);
+		else szyfr+=char(kod-26);
+	}
+	return szyfr;
+}
+int main(){
+	string s;
+	string k;
+	while(weTekst>>s){
+		weKlucz>>k;
+		cout<<szyfrVigener(s,k)<<endl;
+	}
+}
